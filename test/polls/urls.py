0ccb0cdd204f9acfc7dc91e39b1e03ec9839a8e5
tsupljika <ts51828@fer.hr>
2020-12-21 21:10:16 +0100

    Dodavanje funkcija registracije i prijave; promjena izgleda

--- test/polls/urls.py
+++ test/polls/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'polls'
+app_name='polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.home, name='home'),
+    path('register/', views.registerView, name="register"),
+    path('login/', views.loginView, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('index/', views.IndexView.as_view(), name="index"),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
--- test/polls/views.py
+++ test/polls/views.py
@@ -1,11 +1,62 @@
 from django.http import HttpResponseRedirect, HttpResponse, FileResponse
 from .models import Question, Choice
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views import generic
 from django.template.loader import get_template
 from .utils import render_to_pdf
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
+def home(request):
+    context = {}
+    return render(request, 'polls/main.html', context)
+
+def registerView(request):
+    if request.user.is_authenticated:
+        return redirect('polls:index')
+
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account successfully created.')
+            return redirect('polls:login')
+        else:
+            messages.error(request, 'Could not create new user.')
+
+    context = {'form' : form}
+    return render(request, 'polls/register.html', context)
+
+def loginView(request):
+    if request.user.is_authenticated:
+        return redirect('polls:index')
+
+    if request.method == 'POST':
+        username = request.post.get('username')
+        password = request.post.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('polls:index')
+        else:
+            messages.info(request, 'Username or password is incorrect.')
+
+    context = {}
+    return render(request, 'polls/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('polls:home')
+
+@method_decorator(login_required, name='dispatch')
 class ReportView(generic.DetailView):
     model = Question
     template_name = 'polls/report.html'
@@ -25,7 +76,7 @@ def pdfView(request, question_id):
         return response
     return HttpResponse("Not found")
     
-
+@method_decorator(login_required, name='dispatch')
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -34,7 +85,7 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
-
+@method_decorator(login_required, name='dispatch')
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
@@ -44,6 +95,7 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+@login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
